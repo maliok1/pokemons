d8ef289923c389133bcf6bc12c338807f366b0cf
Anastasia Mal <lisanastya15@gmail.com>
2020-05-20 15:50:47 +0200

    Updates

---
+++ precache-manifest.6fe58b6a26ba02adaa69184a9e348991.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5dba3d70390f07f98469ee50d796520",
+    "url": "/pokemons/index.html"
+  },
+  {
+    "revision": "20573a7c71f6d0b512e2",
+    "url": "/pokemons/static/js/2.19df9e90.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/pokemons/static/js/2.19df9e90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8712e602676f10dd25",
+    "url": "/pokemons/static/js/main.a198127d.chunk.js"
+  },
+  {
+    "revision": "6893087a3c10b5e87cef",
+    "url": "/pokemons/static/js/runtime-main.0d533d8a.js"
+  }
+]);
